c48cd8634d849ad5778ddfbc12c7772029bee8a7
Christopher Harrison <geektrainer@github.com>
2024-04-22 10:12:56 -0700

    Revert "Adds runner service"

--- server/services/index.ts
+++ server/services/index.ts
@@ -1,9 +1,7 @@
 import { Service } from "./service";
 import { RaceService } from "./race-service";
-import { RunnerService } from "./runner-service";
 
 export {
     RaceService,
-    RunnerService,
     Service
 }
